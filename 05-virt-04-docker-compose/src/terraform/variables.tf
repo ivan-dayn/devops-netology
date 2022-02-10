c30ef0d4e1353b40b56279f6e0040574ca264631
ivan-dayn <93118042+ivan-dayn@users.noreply.github.com>
2022-02-10 10:14:43 +0300

    Add files via upload

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gnbcocgrie3bmqagpn"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g4p12mos2b6efiud6c"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8mf93iqiaqeeuguf3i"
+}
